93445c2e987e7333dd7f094e3023e67096db2289
Jonathan Dieter <jdieter@lesbg.com>
2015-10-19 10:15:54 +0300

    Fix absolute path lookup

--- src/lesson/main.py
+++ src/lesson/main.py
@@ -17,7 +17,7 @@
 
 import sys, os
 
-abspath = os.path.dirname(__file__)
+abspath = os.path.dirname(os.path.abspath(__file__))
 if abspath not in sys.path:
     sys.path.append(abspath)
 os.chdir(abspath)
